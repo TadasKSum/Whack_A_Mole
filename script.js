de7727f7be882937eb911029d070d878b69a0da9
unknown <tadasksuminas@gmail.com>
2024-05-15 09:52:29 +0300

    Added showMole()

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const game = document.querySelector(".gameBoard")
-const gameHoles = document.querySelectorAll(".cell")
 
+let holesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 let mole = 1;
+
+function showMole() {
+    game.innerHTML = ""
+
+    holesArray.forEach(hole => {
+        const el = document.createElement("div")
+        el.classList.add("cell")
+        el.setAttribute("id", hole)
+        game.append(el)
+    })
+}
+
+showMole()
