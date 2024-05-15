f05a6aeb2226c7994af5b6f802964d09712bf0c1
unknown <tadasksuminas@gmail.com>
2024-05-15 10:05:23 +0300

    Finished showMole()

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const game = document.querySelector(".gameBoard")
 
 let holesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-let mole = 1;
+let mole = 4;
 
 function showMole() {
     game.innerHTML = ""
@@ -9,8 +9,19 @@ function showMole() {
     holesArray.forEach(hole => {
         const el = document.createElement("div")
         el.classList.add("cell")
+        el.classList.add("d-flex")
         el.setAttribute("id", hole)
         game.append(el)
+    })
+
+    const gameHoles = document.querySelectorAll(".cell")
+
+    gameHoles.forEach((cell, index) => {
+        if(mole === Number(cell.id)) {
+            const el = document.createElement("div")
+            el.classList.add("mole")
+            cell.append(el)
+        }
     })
 }
 
