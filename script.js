52636344ab2ab6aa5acff6ae09e9eeb7c9697213
unknown <tadasksuminas@gmail.com>
2024-05-15 10:09:46 +0300

    Added hitMole()

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const game = document.querySelector(".gameBoard")
 
 let holesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 let mole = 4;
+let gamePoints = 0;
 
 function showMole() {
     game.innerHTML = ""
@@ -21,8 +22,17 @@ function showMole() {
             const el = document.createElement("div")
             el.classList.add("mole")
             cell.append(el)
+
+            const moleTarget = document.querySelector(".mole")
+
+            moleTarget.onclick = () => hitMole()
         }
     })
+}
+
+function hitMole() {
+    gamePoints += 10;
+    console.log(gamePoints)
 }
 
 showMole()
