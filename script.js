e905941c59d5a0d821336afc3581193ef8e9f49e
unknown <tadasksuminas@gmail.com>
2024-05-15 10:18:11 +0300

    Finished initial game logic

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const game = document.querySelector(".gameBoard")
+const showPoints = document.querySelector(".points")
 
 let holesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-let mole = 4;
+let mole = 0;
 let gamePoints = 0;
+let moleTimer
 
 function showMole() {
     game.innerHTML = ""
+    showPoints.innerHTML = "Points: "+gamePoints
 
     holesArray.forEach(hole => {
         const el = document.createElement("div")
@@ -35,4 +38,15 @@ function hitMole() {
     console.log(gamePoints)
 }
 
+function popMole() {
+    let molePosition = Math.ceil(Math.random()*9)
+    mole = molePosition;
+}
+
+popMole()
 showMole()
+
+moleTimer = setInterval(() => {
+    popMole()
+    showMole()
+}, 700)
